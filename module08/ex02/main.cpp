9e84be5a49f42fd993399627d024c516e4dc3e79
anthony <anthonyrodriguez.tech@gmail.com>
2024-03-08 11:56:30 +0100

    module08 ex 02 ok

--- module08/ex02/main.cpp
+++ module08/ex02/main.cpp
@@ -24,30 +24,6 @@ void testlist(void)
 	}
 }
 
-void testchar(void)
-{
-	MutantStack<std::string> mstack;
-	mstack.push("Hello");
-	mstack.push("you");
-	std::cout << mstack.top() << std::endl;
-	mstack.pop();
-	std::cout << "Size of stack: " << mstack.size() << std::endl;
-	mstack.push(",");
-	mstack.push(" ");
-	mstack.push("world");
-	mstack.push("!");
-	MutantStack<std::string>::iterator it = mstack.begin();
-	MutantStack<std::string>::iterator ite = mstack.end();
-	++it;
-	--it;
-	while (it != ite)
-	{
-		std::cout << *it;
-		++it;
-	}
-	std::cout << std::endl;
-}
-
 int main()
 {
 	MutantStack<int> mstack;
@@ -72,7 +48,5 @@ int main()
 	std::stack<int> s(mstack);
 	std::cout << std::endl << "Testing a list" << std::endl << std::endl;
 	testlist();
-	std::cout << std::endl << "Testing a string" << std::endl << std::endl;
-	testchar();
 	return (0);
 }
