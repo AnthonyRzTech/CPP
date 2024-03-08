fd922364907ca6634efc08cab7c1dd63103d841d
anthony <anthonyrodriguez.tech@gmail.com>
2024-03-08 11:51:35 +0100

    module09 ex 02 ok

--- module09/ex02/PmergeMe.cpp
+++ module09/ex02/PmergeMe.cpp
@@ -1,81 +1,146 @@
 #include "PmergeMe.hpp"
 
-PmergeMe::PmergeMe(void) {}
-
-PmergeMe::PmergeMe(const PmergeMe &target) { (void)target; }
-
-PmergeMe	&PmergeMe::operator=(const PmergeMe &target) { (void)target; return *this; }
-
-PmergeMe::~PmergeMe(void)
+PmergeMe::PmergeMe() : _size(0)
 {
-	vt.clear();
-	dq.clear();
+	_vector.clear();
+	_list.clear();
 }
 
-int		PmergeMe::jacobsthalNumber(int n)
+PmergeMe::PmergeMe(const std::vector<unsigned int> vector, const std::list<unsigned int> list) :  _size(vector.size()), _vector(vector), _list(list)
 {
-	if (n == 0)
-		return 0;
-	else if (n == 1)
-		return 1;
-	else
-		return jacobsthalNumber(n - 1) + 2 * jacobsthalNumber(n - 2);
 }
 
-void PmergeMe::sort(int argc, char **argv)
+PmergeMe::PmergeMe(const PmergeMe &rhs)
 {
-	std::list<uint64_t>	lt;
+	*this = rhs;
+}
+
+PmergeMe &PmergeMe::operator=(const PmergeMe &rhs)
+{
+	if (this != &rhs)
 	{
-		double	num;
-		char	*ptr;
-	
-		for (int i = 1; i < argc; i++)
+		_vector = rhs._vector;
+		_list = rhs._list;
+	}
+	return *this;
+}
+
+PmergeMe::~PmergeMe()
+{
+	_vector.clear();
+	_list.clear();
+}
+
+void PmergeMe::insertionSort(std::vector<unsigned int> &vector, unsigned int n)
+{
+	std::vector<unsigned int>::iterator it = vector.begin();
+	for (; it != vector.end(); it++)
+	{
+		if (*it > n)
 		{
-			num = strtod(argv[i], &ptr);
-			if (*ptr != '\0'
-					|| (static_cast<uint64_t>(num) != num)
-					|| (num == 0 && !isdigit(*(argv[i]))))
-				throw std::runtime_error("Error");
-			vt.push_back(num);
-			dq.push_back(num);
-			lt.push_back(num);
+			vector.insert(it, n);
+			return;
 		}
 	}
-	std::cout << "before:\t";
-	for (std::list<uint64_t>::iterator it = lt.begin(); it != --lt.end(); it++)
-		std::cout << *it << " ";
-	std::cout << *(--lt.end()) << std::endl;
-	lt.sort();
-	std::cout << "after:\t";
-	for (std::list<uint64_t>::iterator it = lt.begin(); it != --lt.end(); it++)
-		std::cout << *it << " ";
-	std::cout << *(--lt.end()) << std::endl;
+	vector.push_back(n);
+}
 
-	int size = argc - 1;
-	int node;
-
-	clock_t	dq_bf = clock();
-	node = this->splitPair(1, size, dq);
-	this->insertMerge(size, node, dq);
-	clock_t	dq_af = clock();
-
-	clock_t	vt_bf = clock();
-	node = this->splitPair(1, size, vt);
-	this->insertMerge(size, node, vt);
-	clock_t	vt_af = clock();
-
-	std::vector<uint64_t>::iterator vit = vt.begin();
-	std::deque<uint64_t>::iterator dit = dq.begin();
-	for (std::list<uint64_t>::iterator lit = lt.begin(); lit != lt.end(); lit++)
+void PmergeMe::insertionSort(std::list<unsigned int> &list, unsigned int n)
+{
+	std::list<unsigned int>::iterator it = list.begin();
+	for (; it != list.end(); it++)
 	{
-		if (*lit != *dit || *lit != *vit)
+		if (*it > n)
 		{
-			std::cerr << "not sort!!" << std::endl;
-			return ;
+			list.insert(it, n);
+			return;
 		}
-		vit++;
-		dit++;
 	}
-	std::cout << "Time to process a range of\t" << size << " elements with std::vector : " << 1000.0 * (vt_af - vt_bf) / CLOCKS_PER_SEC << " ms" << std::endl;
-	std::cout << "Time to process a range of\t" << size << " elements with std::deque  : " << 1000.0 * (dq_af - dq_bf) / CLOCKS_PER_SEC << " ms" << std::endl;
+	list.push_back(n);
+}
+
+void PmergeMe::vectorMergeInsertionSort()
+{
+	std::vector<unsigned int> small, large;
+	unsigned int a, b, min, max;
+	while (!_vector.empty())
+	{
+		a = _vector.back();
+		_vector.pop_back();
+		if (_vector.empty())
+		{
+			small.push_back(a);
+			break;
+		}
+		b = _vector.back();
+		_vector.pop_back();
+		a > b ? (min = b, max = a) : (min = a, max = b);
+		small.push_back(min);
+		insertionSort(large, max);
+	}
+	std::vector<unsigned int>::iterator it = small.begin();
+	for (; it != small.end(); it++)
+		insertionSort(large, *it);
+	_vector = large;
+}
+
+void PmergeMe::listMergeInsertionSort()
+{
+	std::list<unsigned int> small, large;
+	unsigned int a, b, min, max;
+	while (!_list.empty())
+	{
+		a = _list.back();
+		_list.pop_back();
+		if (_list.empty())
+		{
+			small.push_back(a);
+			break;
+		}
+		b = _list.back();
+		_list.pop_back();
+		a > b ? (min = b, max = a) : (min = a, max = b);
+		small.push_back(min);
+		insertionSort(large, max);
+	}
+	std::list<unsigned int>::iterator it = small.begin();
+	for (; it != small.end(); it++)
+		insertionSort(large, *it);
+	_list = large;
+}
+
+void PmergeMe::mergeInsertionSort()
+{
+	clock_t start, end;
+	double vectorTime, listTime;
+	std::cout << "Before: ";
+	getVector();
+	start = clock();
+	vectorMergeInsertionSort();
+	end = clock();
+	vectorTime = (double)(end - start) / CLOCKS_PER_SEC * 1000.0;
+	start = clock();
+	listMergeInsertionSort();
+	end = clock();
+	listTime = (double)(end - start) / CLOCKS_PER_SEC * 1000.0;
+	std::cout << "After:  ";
+	getVector();
+	std::cout << "Time to process a range of " << _size << " elements with std::vector : " << vectorTime << " ms" << std::endl;
+	std::cout << "Time to process a range of " << _size << " elements with std::list   : " << listTime << " ms" << std::endl;
+}
+
+void PmergeMe::getVector() const
+{
+	std::vector<unsigned int>::const_iterator it = _vector.begin();
+	for (; it != _vector.end(); it++)
+		std::cout << *it << " ";
+	std::cout << std::endl;
+}
+
+void PmergeMe::getList() const
+{
+	std::list<unsigned int>::const_iterator it = _list.begin();
+	for (; it != _list.end(); it++)
+		std::cout << *it << " ";
+	std::cout << std::endl;
 }
--- module09/ex02/main.cpp
+++ module09/ex02/main.cpp
@@ -1,20 +1,28 @@
 #include "PmergeMe.hpp"
 
-
-int	main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	try {
-		if (argc - 1 < 1)
-			throw std::runtime_error("Error");
-
-		PmergeMe	pm;
-		pm.sort(argc, argv);
-	} catch (std::exception &e) {
-
-		std::cerr << e.what() << std::endl;
+	if (argc == 1)
+	{
+		std::cerr << "Error: no arguments" << std::endl;
 		return 1;
-
 	}
-
+	std::vector<unsigned int> vector;
+	std::list<unsigned int> list;
+	int	num;
+	for (int i = 1; i < argc; i++)
+	{
+		std::istringstream iss(argv[i]);
+		iss >> num;
+		if (iss.fail() || !iss.eof() || num < 0)
+		{
+			std::cerr << "Error: invalid argument" << std::endl;
+			return 1;
+		}
+		vector.push_back(num);
+		list.push_back(num);
+	}
+	PmergeMe p(vector, list);
+	p.mergeInsertionSort();
 	return 0;
 }
